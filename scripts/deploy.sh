4e4fba8ed4cc67bb29aeef872e20261283e9ab89
David Bowland <dbowland1@gmail.com>
2022-02-22 00:06:29 -0600

    Use roles for pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,13 +14,14 @@ sam build --template ${SAM_TEMPLATE} --use-container
 
 # Deploy build lambda
 
-TESTING_ARTIFACTS_BUCKET=aws-sam-cli-managed-sms-queue-ser-artifactsbucket-1kcrbw8sd5lhn
-TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-sms-q-CloudFormationExecutionR-1MYOQZK95XHZX"
+TESTING_ARTIFACTS_BUCKET=dbowland-lambda-east-1-test
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/dbowland-cloudformation-test"
 TESTING_STACK_NAME=sms-queue-service-test
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
            --region us-east-1 \
            --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --s3-prefix sms-queue-service-test \
            --no-fail-on-empty-changeset \
            --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
-           --parameter-overrides "AccountId=$AWS_ACCOUNT_ID Environment=test"
+           --parameter-overrides "Environment=test"
