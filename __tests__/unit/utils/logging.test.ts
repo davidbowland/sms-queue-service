4cdb9bc9eff9c3fc33fe15adb26c1bc087f48e38
David Bowland <dbowland1@gmail.com>
2023-04-18 02:53:02 -0500

    Add type checking to push hook

--- __tests__/unit/utils/logging.test.ts
+++ __tests__/unit/utils/logging.test.ts
@@ -6,17 +6,9 @@ import { PinpointClient } from '@aws-sdk/client-pinpoint'
 jest.mock('aws-xray-sdk-core')
 
 describe('logging', () => {
-  const consoleError = console.error
-  const consoleLog = console.log
-
   beforeAll(() => {
     console.error = jest.fn()
     console.log = jest.fn()
-  })
-
-  afterAll(() => {
-    console.error = consoleError
-    console.log = consoleLog
   })
 
   describe('log', () => {
